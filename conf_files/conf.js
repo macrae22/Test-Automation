33551ec086f4be2bd40d419f1445ddb0d8b955fc
AzureAD\MartynMacrae <martyn.macrae@bud.co.uk>
2017-09-13 15:07:55 +0100

    Added node modules
    Minor amends to 'local.conf.js' file to maximise browser

--- conf_files/conf.js
+++ conf_files/conf.js
@@ -1,8 +1,6 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['C:/Users/MartynMacrae/Test-Automation/tests/smoketests.js'],
-  
-  //restartBrowserBetweenTests: true,
 
   capabilities: {
     'browserName': 'chrome',
--- conf_files/local.conf.js
+++ conf_files/local.conf.js
@@ -1,13 +1,23 @@
 var browserstack = require('browserstack-local');
 
 exports.config = {
-  'seleniumAddress': 'http://hub-cloud.browserstack.com/wd/hub',
+  specs: ['C:/Users/MartynMacrae/Test-Automation/tests/smoketests.js'],
+  seleniumAddress: 'http://hub-cloud.browserstack.com/wd/hub',
 
   'capabilities': {
     'browserstack.user': 'jasonbunting1',
     'browserstack.key': 'SxEqcP3hpczft7bFpEBp',
     'browserstack.local': true,
-    'browserName': 'chrome'
+    'os': 'Windows',
+    'os_version': '8',
+    'browserName': 'Chrome',
+    'browser_version': '60.0',
+    'resolution': '1280x800',
+    'browserstack.debug': true
+  },
+
+  onPrepare : function() {
+    browser.manage().window().maximize();
   },
 
   // Code to start browserstack local before start of test
